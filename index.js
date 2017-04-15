66ae9bd9493d867b5fa3456f5c9728e01b16b4d1
Nic <nic@neap.co>
2017-04-15 22:33:42 +1000

    Update message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.helloWorld = function helloWorld(req, res) {
   // Example input: {"message": "Hello!"}
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required.
-    res.status(400).send('Hello guys!');
+    res.status(400).send('Hello Google!');
   } else {
     // Everything is okay.
     console.log(req.body.message);
